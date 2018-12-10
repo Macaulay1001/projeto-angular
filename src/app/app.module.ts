c988d40dc03ee95f23a0644e71eaecd447767d0b
Macaulay1001 <macaulayhazza@gmail.com>
2018-12-10 11:47:16 -0200

    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppBootstrapModule } from './app-bootstrap/app-bootstrap.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppBootstrapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
